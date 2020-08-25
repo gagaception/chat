eec8436da68615b8010b55d83b3f9a4cf02c658c
gagaception <sharipov.aziz.sh@gmail.com>
2020-08-25 18:15:00 +0600

    WTF commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'chat#show'
+  
+  get "/login",  to: "auth#new"
+  post "/login", to: "auth#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
